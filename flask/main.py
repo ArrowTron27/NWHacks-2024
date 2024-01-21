bc9d307ed5ddd5c475e3552246406868dcb2fc23
ArrowTron27 <134024669+ArrowTron27@users.noreply.github.com>
2024-01-20 16:06:38 -0800

    data gets sent back to the html

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, HiddenField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
@@ -10,6 +10,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'NWHacksLoser'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
+form_submitted = None
+
 class UploadFileForm(FlaskForm):
     # file = FileField("File", validators=[
     #     FileRequired(),
@@ -17,24 +19,28 @@ class UploadFileForm(FlaskForm):
     #     ])
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
+    form_submitted = HiddenField("form_submitted")
 
 
 @app.route('/', methods=['GET',"POST"])
 @app.route('/home', methods=['GET',"POST"])
 def home():
     form = UploadFileForm()
-    data = None
+    goodjob = False
+
     if form.validate_on_submit():
         # Grab the file
         file = form.file.data 
         # Save the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
         # Call douglases function
-        # function would return something
-        data = "some data"
-        return "File has been uploaded"
-    #return render_template('index.html', data=data)
-    return render_template('index.html', form=form)
+        # function would return something, the goodjob what should be returned
+        goodjob = False
+        form.form_submitted.data = True
+        # render_template('index.html', data=data)
+        # return "File has been uploaded"
+
+    return render_template('index.html', form=form, goodjob=goodjob)
 
 if __name__ == '__main__':
     app.run(debug=True)
